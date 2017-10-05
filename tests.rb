188007c581893e5edaa768b2923c7b99d88c4b67
Jett Crowson <jettcrowson@gmail.com>
2017-10-05 11:49:39 -0600

    Testing for mode

--- tests.rb
+++ tests.rb
@@ -27,8 +27,17 @@ describe 'number_summary' do
         it 'finds the standard deviation' do
             standard_deviation([6, 2, 3, 1]).round(2).must_equal(1.87)
             standard_deviation([1,1,1,1,1,1,1,1]).round(2).must_equal(0)
+            standard_deviation([1]).round(2).must_equal(0)
             standard_deviation([9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4]).round(2).must_equal(2.98)
         end
+    end
+
+    describe 'mode' do
+        it 'finds the most common occuring number(s)' do
+            mode([1,1,2,3,4]).must_include(1)
+            mode([1,1,2,2,3,3,3,4,4,4]).must_include(3,4)
+            mode([1]).must_include(1)
+        end 
     end
 
 end
