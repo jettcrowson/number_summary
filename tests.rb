c4ac2476b5b28ed981b9125fa696e7b5227c2a10
Jett Crowson <jettcrowson@gmail.com>
2017-10-05 11:26:39 -0600

    Add testing for median

--- tests.rb
+++ tests.rb
@@ -8,11 +8,17 @@ require './number_summary'
 describe 'number_summary' do
 
     describe 'mean' do
-
         it 'returns the mean' do
-            mean([1,2,3,4,5]).must_equal(3);
+            mean([1,2,3,4,5]).must_equal(3)
+            mean([1,2,3,4]).must_equal(2.5)
         end
+    end
 
+    describe 'median' do
+        it 'finds the middle number' do
+            median([1,3,7]).must_equal(3)
+            median([1,2,3,4]).must_equal(2.5)
+        end
     end
 
 end
